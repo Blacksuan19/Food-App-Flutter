c30e3108fb2d31f045c08c127b67ea2530798eb0
blacksuan19 <abubakaryagob@gmail.com>
2021-03-06 20:56:02 +0800

    category: add selected background

--- lib/widgets/category_row.dart
+++ lib/widgets/category_row.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
 
 class CategoryRow extends StatelessWidget {
-  Column makeCategory({assetName}) {
-    return Column(
-      children: [
-        Image(
-          image: AssetImage("assets/images/$assetName.png"),
-          height: 60,
-          width: 60,
-        ),
-        Text(
-          // capitalize the first letter
-          assetName[0].toUpperCase() + assetName.substring(1),
-          style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w500,
-              color: Colors.grey[500]),
-        )
-      ],
+  Container makeCategory({assetName, isSelected = false}) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 10),
+      decoration: isSelected
+          ? BoxDecoration(
+              color: Color(0xFFFBFBFB),
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.yellow[200].withOpacity(0.5),
+                  Colors.red[100].withOpacity(0.5),
+                ],
+              ),
+            )
+          : BoxDecoration(),
+      child: Column(
+        children: [
+          Image(
+            image: AssetImage("assets/images/$assetName.png"),
+            height: 60,
+            width: 60,
+          ),
+          Text(
+            // capitalize the first letter
+            assetName[0].toUpperCase() + assetName.substring(1),
+            style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[500]),
+          )
+        ],
+      ),
     );
   }
 
@@ -30,7 +49,7 @@ class CategoryRow extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           makeCategory(assetName: "pizza"),
-          makeCategory(assetName: "burger"),
+          makeCategory(assetName: "burger", isSelected: true),
           makeCategory(assetName: "taco"),
           makeCategory(assetName: "cupcake"),
           makeCategory(assetName: "pasta"),
