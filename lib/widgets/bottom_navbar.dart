9454f2a12203b001c5ad218a901698bbce573b48
blacksuan19 <abubakaryagob@gmail.com>
2021-03-06 19:01:40 +0800

    bottom_navbar: convert to a widget
    makes sense this way

--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
 
-BottomNavigationBar makeBottomNav() {
-  return BottomNavigationBar(
-    showSelectedLabels: false,
-    showUnselectedLabels: false,
-    items: [
-      makeItem(assetName: "home"),
-      makeItem(assetName: "star"),
-      // the icon is smaller so pass larger size to fit with others
-      makeItem(assetName: "cart", size: 20.0),
-      makeItem(assetName: "profile"),
-    ],
-    iconSize: 15,
-  );
-}
-
-BottomNavigationBarItem makeItem({assetName, size}) {
-  return BottomNavigationBarItem(
-    label: "",
-    icon: IconButton(
-      icon: Image(
-        image: AssetImage("assets/images/$assetName.png"),
-        height: size ?? 16,
-        width: size ?? 16,
+class NavBar extends StatelessWidget {
+  BottomNavigationBarItem makeItem({assetName, size}) {
+    return BottomNavigationBarItem(
+      label: "",
+      icon: IconButton(
+        icon: Image(
+          image: AssetImage("assets/images/$assetName.png"),
+          height: size ?? 16,
+          width: size ?? 16,
+        ),
+        onPressed: () => {},
       ),
-      onPressed: () => {},
-    ),
-  );
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      items: [
+        makeItem(assetName: "home"),
+        makeItem(assetName: "star"),
+        // the icon is smaller so pass larger size to fit with others
+        makeItem(assetName: "cart", size: 20.0),
+        makeItem(assetName: "profile"),
+      ],
+      iconSize: 15,
+    );
+  }
 }
