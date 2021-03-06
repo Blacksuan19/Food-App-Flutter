5db1d484b06f10f70c4e8f932a66cc731dbfb5fc
blacksuan19 <abubakaryagob@gmail.com>
2021-03-06 18:38:06 +0800

    nav_bar: use iconbutton instead of icon

--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -7,17 +7,24 @@ BottomNavigationBar makeBottomNav() {
     items: [
       makeItem(assetName: "home"),
       makeItem(assetName: "star"),
-      makeItem(assetName: "cart"),
+      // the icon is smaller so pass larger size to fit with others
+      makeItem(assetName: "cart", size: 20.0),
       makeItem(assetName: "profile"),
     ],
+    iconSize: 15,
   );
 }
 
-BottomNavigationBarItem makeItem({assetName}) {
+BottomNavigationBarItem makeItem({assetName, size}) {
   return BottomNavigationBarItem(
     label: "",
-    icon: Image(
-      image: AssetImage("assets/images/$assetName.png"),
+    icon: IconButton(
+      icon: Image(
+        image: AssetImage("assets/images/$assetName.png"),
+        height: size ?? 16,
+        width: size ?? 16,
+      ),
+      onPressed: () => {},
     ),
   );
 }
