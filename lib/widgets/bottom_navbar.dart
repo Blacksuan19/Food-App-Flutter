ec2d5986aed1c9343b03ca9727e2f1c334b69e1b
blacksuan19 <abubakaryagob@gmail.com>
2021-03-06 20:56:30 +0800

    navbar: correct style

--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -17,17 +17,37 @@ class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      items: [
-        makeItem(assetName: "home"),
-        makeItem(assetName: "star"),
-        // the icon is smaller so pass larger size to fit with others
-        makeItem(assetName: "cart", size: 20.0),
-        makeItem(assetName: "profile"),
-      ],
-      iconSize: 15,
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFFFBFBFB),
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(20),
+          topLeft: Radius.circular(20),
+        ),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.grey[600], Colors.white],
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+        child: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            makeItem(assetName: "home"),
+            makeItem(assetName: "star"),
+            // the icon is smaller so pass larger size to fit with others
+            makeItem(assetName: "cart", size: 20.0),
+            makeItem(assetName: "profile"),
+          ],
+          iconSize: 15,
+        ),
+      ),
     );
   }
 }
