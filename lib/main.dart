98141949aa8e22f844e4b902291f313652ce0b91
blacksuan19 <abubakaryagob@gmail.com>
2021-03-07 15:25:22 +0800

    lib: Generate Barrel files

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:food_app_ui/screens/home.dart';
+import 'package:food_app_ui/screens/screens.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,6 +1,6 @@
-export '/widgets/app_bar.dart';
-export '/widgets/bottom_navbar.dart';
-export '/widgets/category_row.dart';
-export '/widgets/item_card.dart';
-export '/widgets/text_field.dart';
-export '/widgets/header.dart';
+export 'app_bar.dart';
+export 'bottom_navbar.dart';
+export 'category_row.dart';
+export 'header.dart';
+export 'item_card.dart';
+export 'text_field.dart';
